2e6cfa55fd3c0af42afc2731be4429107fc3ad51
Zander Goodwin <zanderngoodwin@gmail.com>
2025-11-17 16:36:35 -0400

    Timeline done

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Trigger : MonoBehaviour
+{
+    public PlayableDirector timeLine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Trigger"))
+        {
+            timeLine.Play();
+            Debug.Log("FUCKKKKKKK");
+        }
+    }
+}
